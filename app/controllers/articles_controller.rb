7ef51a23a98a940ce2f2ad0cfd4d3604b733a52d
Arthur Chen <arthur.chen@intramirror.com>
2018-03-07 14:52:07 +0000

    complete Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,18 @@
 class ArticlesController < ApplicationController
-
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
+    def index
+       @articles = Article.all 
+    end
+    
     def new
         @article = Article.new
     end
     
     def show
-        @article = Article.find(params[:id])
+    end
+    
+    def edit
     end
     
     def create
@@ -20,9 +27,31 @@ class ArticlesController < ApplicationController
         
     end
     
+    def update
+       
+       if @article.update(article_params)
+           flash[:notice] = "Article was successfully updated"
+           redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     
+    end
+    
+    def destroy
+       
+       @article.destroy
+       flash[:notice] = 'Article was fully deleted'
+       
+       redirect_to articles_path
+    end
     
     private
+    
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
        params.require(:article).permit(:title, :description)
     end
